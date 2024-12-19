f7c8f5354f5d56c467e9eedf599548334b0ade16
SAI RAM REDDY THAMMANENI <166827331+sairamthammaneni@users.noreply.github.com>
2024-12-19 14:31:51 +0530

    Update Moviebot.js

--- JAVA SCRIPT/Moviebot.js
+++ JAVA SCRIPT/Moviebot.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = 'TELGRAM_BOT_TOKEN';
+const token = 'TELGRAM_BOT_TOKEN_HERE';
 
 const bot = new TelegramBot(token, {polling: true});
 
@@ -12,7 +12,7 @@ bot.on('message',(msg)=>{
         bot.sendMessage(msg.chat.id,"Hello,I am a Movie bot, Please Enter a movie name : ")
     }
     else{
-        request('http://www.omdbapi.com/?t='+msg.text+'&apikey=API_KEY', function (error, response, body){
+        request('http://www.omdbapi.com/?t='+msg.text+'&apikey=API_KEY_HERE', function (error, response, body){
             if(JSON.parse(body).response=="False"){
                 bot.sendMessage(msg.chat.id, "Data not found")
             }
